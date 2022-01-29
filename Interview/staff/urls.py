fd45c236e1be77865c887e31c572b626b1602672
Jude <87612608+jude52@users.noreply.github.com>
2022-01-29 18:04:16 +0000

    commiting create_field.html and updated views / paths un url.py

--- Interview/staff/urls.py
+++ Interview/staff/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home)
+    path("", views.home),
+    path("staff/input.html", views.TestView.as_view(), name='input'),
+    path("staff/create_field.html", views.CreateTestView.as_view(), name='create'),
+    
 ]
--- Interview/staff/views.py
+++ Interview/staff/views.py
@@ -1,6 +1,25 @@
+from dataclasses import fields
 from django.shortcuts import render
+from django.template import context
+from .models import StaffInfo
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 # Create your views here.
 def home(request):
     context = {"company": "NESCOLL"}
     return render(request, "staff/home.html", context)
+
+ 
+class TestView(ListView):
+    model = StaffInfo
+    template_name = "staff/input.html"
+
+class CreateTestView(CreateView):
+    model = StaffInfo
+    template_name = "staff/create_field.html"
+    fields = [
+       'first_name', 
+        'last_name', 'department', 'job_title', 'start_date', 'end_date'
+
+    ]
